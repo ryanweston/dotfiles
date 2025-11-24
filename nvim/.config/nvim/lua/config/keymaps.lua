8a56b76aed7c2012101f150b405395a6ebc45b87
Ryan <ryanlweston@gmail.com>
2025-11-24 15:20:13 +0000

    feat(nvim): add `tiny-inline-diagnostics`

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -2,6 +2,3 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 -- In your keybinding setup (like which-key configuration)
-vim.keymap.set("n", "K", function()
-  vim.lsp.buf.hover({ border = "single", max_height = 25, max_width = 120 })
-end, { desc = "Hover documentation" })
