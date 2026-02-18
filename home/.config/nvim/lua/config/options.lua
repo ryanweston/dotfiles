af66065ba86e6f164f19b109fcdc9650e6ba3074
Ryan <ryanlweston@gmail.com>
2026-02-18 20:51:45 +0000

    feat: add nvim options

--- home/.config/nvim/lua/config/options.lua
+++ home/.config/nvim/lua/config/options.lua
@@ -1,4 +1,24 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.opt.relativenumber = false
+
+-- Disable automatic line wrapping
+vim.opt.wrap = false
+
+-- Enable smart auto-indenting
+vim.opt.smartindent = true
+vim.opt.shiftwidth = 2
+
+-- Indent wrapped likes visually
+vim.opt.breakindent = true
+
+-- Enable relative line numbers:wrapping
+vim.opt.nu = true
+vim.opt.rnu = true
+
+-- Enable ignorecase + smartcase for better search
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Enable persistant undo history
+vim.opt.undofile = true
